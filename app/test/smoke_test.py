32ba9cfa4e1b60b0e8a7c8f4b5d34539eb859934
eric <gowest.wang@gmail.com>
2025-11-26 21:39:55 -0800

    1.add user management playwright/pytest test cases

--- app/test/smoke_test.py
+++ app/test/smoke_test.py
@@ -9,16 +9,17 @@ playwright = sync_playwright().start()
 browser = playwright.chromium.launch(headless=False)
 
 #browser = playwright.chromium.connect("ws://playwright-service:3000")
-context = browser.new_context()
+
+context = browser.new_context(viewport={'width': 1024, 'height': 768})
 page = context.new_page()
 page.goto(URL)
-def test_login():
 
-    page.get_by_placeholder("Enter username").click()
-    page.get_by_placeholder("Enter username").fill("admin@admin.com")
-    page.get_by_placeholder("Enter username").press("Tab")
-    page.get_by_placeholder("Enter password").fill("1234")
-    page.get_by_role("button", name="Login").click()
+page.get_by_placeholder("Enter username").click()
+page.get_by_placeholder("Enter username").fill("admin@admin.com")
+page.get_by_placeholder("Enter username").press("Tab")
+page.get_by_placeholder("Enter password").fill("1234")
+page.get_by_role("button", name="Login").click()
+
 def test_a_dashboard_new_ticket():
     page.get_by_role("button", name="+ CREATE NEW TICKEY +").click()
     page.locator("input[name=\"ticket_title\"]").click()
@@ -31,13 +32,39 @@ def test_a_dashboard_new_ticket():
 def test_a_dashboard_edit_ticket():
     page.locator("//div[text()='gui-new-ticket']/following-sibling::button[1]").click()
 
-    #page.locator("input[name=\"ticket_title\"]").click()
+    page.locator("input[name=\"ticket_title\"]").click()
     page.locator("input[name=\"ticket_title\"]").fill("edit-title")
+    page.wait_for_timeout(1000)
     page.get_by_role("button", name="Save Changes").click()
+    page.wait_for_timeout(1000)
+    #page.locator("//div[text()='edit-title']/following-sibling::button[1]").click()
+@pytest.mark.user_management
+def test_user_management_new():
+    page.get_by_role("link", name="User Management").click()
+    page.get_by_role("button", name="➕ Add User").click()
+    page.locator("input[name=\"user_name\"]").click()
+    page.locator("input[name=\"user_name\"]").fill("new-user")
+    page.locator("input[name=\"user_name\"]").press("Tab")
+    page.locator("input[name=\"email\"]").fill("new-user@email.com")
+    page.locator("input[name=\"user_password\"]").click()
+    page.locator("input[name=\"user_password\"]").fill("1234")
+    page.get_by_role("button", name="Add", exact=True).click()
 
-    page.locator("//div[text()='dit-title']/following-sibling::button[1]").click()
+@pytest.mark.user_management
+def test_user_management_edit():
 
+    new_user_line = page.locator("//td[text()='new-user@email.com']/following-sibling::td[2]")
+    new_user_line.locator("//button[text()='Edit']").click()
 
+    #print("======",get_new_user)
+   #
+    page.locator("input[name=\"user_name\"]").click()
+    page.locator("input[name=\"user_name\"]").fill("new-user-edit")
+    page.get_by_role("spinbutton").click()
+    page.get_by_role("spinbutton").fill("2")
+    page.get_by_role("button", name="Save Changes").click()
+    #page.once("dialog", lambda dialog: dialog.dismiss())
+    #page.get_by_role("row", name="89 new-user-ed
     # ---------------------
     context.close()
     browser.close()
